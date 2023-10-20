d717f9fc9a3f2ec9adf317ceb157246316966ec3
Mahmudur <mahmudhera93@gmail.com>
2023-10-20 04:50:19 -0400

    experimented with kmer.h, have a sample ready for mmh3

--- test_frac_kmc/kmer.h
+++ test_frac_kmc/kmer.h
@@ -632,7 +632,7 @@ std::string CKmer<1>::get_string_representation()
 {
     std::string ret_str = "";
 	unsigned long long data_copy = data;
-    for (uint32 i = KMER_SIZE-1; i>=0; i++)
+    for (uint32 i = 16; i>=0; i--)
     {
 		uint32 x = data_copy & 0x03;
 		data_copy = data_copy >> 2;
