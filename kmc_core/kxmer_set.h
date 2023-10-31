1e8d3afb4dbe9842104b154c50629ec132dcbf79
Mahmudur <mahmudhera93@gmail.com>
2023-10-31 02:00:00 -0400

    show few more things

--- kmc_core/kxmer_set.h
+++ kmc_core/kxmer_set.h
@@ -689,10 +689,10 @@ public:
 		this->seed = seed;
 		this->scaled = scaled;
 		threshold = (long double)(largest_value)/(long double)(scaled);
-		std::cout << "threshold: " << threshold << std::endl;
-		std::cout << "largest_value: " << largest_value << std::endl;
-		std::cout << "scaled: " << scaled << std::endl;
-		std::cout << "seed: " << seed << std::endl;
+		//std::cout << "threshold: " << threshold << std::endl;
+		//std::cout << "largest_value: " << largest_value << std::endl;
+		//std::cout << "scaled: " << scaled << std::endl;
+		//std::cout << "seed: " << seed << std::endl;
 	}
 
 	void operator()()
